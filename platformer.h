0b81d6722e8c7caf7f2a2ea00a152538a847d80c
RandyGaul <1919825+RandyGaul@users.noreply.github.com>
2019-03-03 15:13:05 -0800

    asdfas

--- platformer.h
+++ platformer.h
@@ -1,4 +1,32 @@
+#ifndef PLATFORMER_H
+#define PLATFORMER_H
+
+struct player2d_t
+{
+	v2 pos;
+	aabb_t box;
+	capsule_t capsule;
+};
+
+/*
+	is on ground
+	is on edge
+	swap from capsule (moving) to box (standing)
+	works for concave situations
+
+	wall jumping
+	wall sliding
+	corner grabbing
+	double jumping
+
+	friction on ground
+	different ground friction values
+	player friction
+	mixing the player friction with the ground friction
+*/
 
 void InitPlatformer();
 
 void UpdatePlatformer(float dt);
+
+#endif // PLATFORMER_H
--- player2d.h
+++ player2d.h
@@ -1,28 +1,4 @@
 #ifndef PLAYER2D_H
 #define PLAYER2D_H
 
-struct player2d_t
-{
-	v2 pos;
-	aabb_t box;
-	capsule_t capsule;
-};
-
-/*
-	is on ground
-	is on edge
-	swap from capsule (moving) to box (standing)
-	works for concave situations
-
-	wall jumping
-	wall sliding
-	corner grabbing
-	double jumping
-
-	friction on ground
-	different ground friction values
-	player friction
-	mixing the player friction with the ground friction
-*/
-
 #endif // PLAYER2D_H
